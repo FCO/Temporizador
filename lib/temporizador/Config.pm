6c00c1539f9b32ee17065f9837ac6e633ba509fb
Breno G. de Oliveira <garu@cpan.org>
2009-07-24 16:09:47 -0300

    micro documentacao para temporizador:config

--- lib/temporizador/Config.pm
+++ lib/temporizador/Config.pm
@@ -4,6 +4,17 @@ use strict;
 use warnings;
 use Carp;
 
+=pod 
+
+=head1 new
+
+   my $conf = temporizador::Config->new( $opcao1, $opcao2, $opcao3, ...)
+
+instancia novo arquivo de configuracao. Pode receber uma lista de elementos como opcoes.
+
+=cut
+
+
 sub new {
    my $class   = shift;
    my @options = @_;
@@ -16,6 +27,18 @@ sub new {
    $self;
 }
 
+=pod
+
+=head1 load_config
+
+   $conf->load_config('arquivo.conf')
+   $conf->load_config('arq1.conf', 'arq2.conf', ...)
+
+carrega um ou mais arquivos de configuracao passados como parametro. 
+Retorna o proprio objeto, para facilitar encadeamento.
+
+=cut
+
 sub load_config {
    my $self  = shift;
    my @files = @_;
@@ -25,6 +48,18 @@ sub load_config {
    $self;
 }
 
+=pod 
+
+=head1 config
+
+  my $valor = $conf->config('item');  # getter
+  
+  $conf->config('item') = $valor;     # setter
+
+Acessor principal para valores dentro de arquivos de configuracao
+
+=cut 
+
 sub config :lvalue {
    my $self = shift;
    my $opt  = shift;
@@ -33,6 +68,12 @@ sub config :lvalue {
    $self->{conf}->{$opt} = "" unless exists $self->{conf}->{$opt};
    $self->{conf}->{$opt}
 }
+
+=pod
+
+=head1 confs_by_files
+
+=cut
 
 sub confs_by_files {
    my $self = shift;
@@ -50,6 +91,16 @@ sub confs_by_files {
    \%conf
 }
 
+=pod
+
+=head1 save_config
+
+  $conf->save_config('arquivo.conf');
+
+Grava as configuracoes de $conf no arquivo especificado.
+
+=cut
+
 sub save_config {
    my $self = shift;
    my $file = shift;
@@ -65,9 +116,6 @@ sub save_config {
    }
    $self;
 }
-
-
-
 
 
 return 42
